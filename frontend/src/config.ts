1925adab309b4464d835ae1de92fb1defbdb59c0
DanielMendozaC <Danielmcarbono@gmail.com>
2025-05-07 22:59:27 -0700

    Complete Kubernetes deployment with environment configuration

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -5,7 +5,7 @@
 // export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 // Changed to refer to the deployed backend
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://api.allorigins.win/raw?url=https://34-8-10-114.nip.io';
+export const API_BASE_URL = process.env.REACT_APP_API_URL || '';  
 
 export const DISPLAY_TO_API_MAPPING = {
   'Push-ups': 'pushups'
